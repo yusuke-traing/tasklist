cc04ed96eb948a72bab05192b83d5dc7eef3319f
yusuke.iga <yusuke_iga@r.recruit.co.jp>
2021-07-26 11:53:24 +0900

    showの作成

--- src/main/java/controllers/ShowServlet.java
+++ src/main/java/controllers/ShowServlet.java
@@ -2,11 +2,16 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class ShowServlet
@@ -26,8 +31,20 @@ public class ShowServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 該当のIDのタスクを取得
+        Task task = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        // タスクをリクエストスコープに格納
+        request.setAttribute("task", task);
+
+        // viewを表示
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/show.jsp");
+        rd.forward(request, response);
+
     }
 
 }
