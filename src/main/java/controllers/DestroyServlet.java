b589f0896aa1829e4e668ae0317f6132b3619ae6
yusuke.iga <yusuke_iga@r.recruit.co.jp>
2021-07-26 12:13:42 +0900

    destroyの作成

--- src/main/java/controllers/DestroyServlet.java
+++ src/main/java/controllers/DestroyServlet.java
@@ -2,11 +2,15 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class DestroyServlet
@@ -27,8 +31,25 @@ public class DestroyServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())){
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからIDを取得して該当のIDのタスクを取得
+            Task task = em.find(Task.class, (Integer)(request.getSession().getAttribute("message_id")));
+
+            em.getTransaction().begin();
+            em.remove(task);
+            em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "削除が完了しました。");
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("task_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
     }
 
 }
